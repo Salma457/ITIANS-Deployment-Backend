ceb5c8ae32163beff138d7cda1bc1caf954f431d
Aliaa <aliaaalihesh@gmail.com>
2025-06-21 20:25:55 +0300

    chat

--- app/Http/Controllers/Employer/EmployerJobController.php
+++ app/Http/Controllers/Employer/EmployerJobController.php
@@ -14,6 +14,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 class EmployerJobController extends Controller
 {
+    
     public function index(\Illuminate\Http\Request $request)
 {
     $query = Job::with(['employer', 'statusChanger']);
